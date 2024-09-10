f0dbe43c83b4041b6e412fab2933e0de33446d5b
Amar Kulkarni <128909990+amarlrl@users.noreply.github.com>
2024-09-10 16:40:02 +0530

    Add files via upload

---
+++ DuplicateContactException.java
@@ -0,0 +1,11 @@
+package Miniproj;
+
+public class DuplicateContactException extends Exception {
+
+
+	public DuplicateContactException(String message) {
+		// TODO Auto-generated constructor stub
+		super(message);
+	}
+}
+
